fb7d6b0104092156a4a05aa6dc7a0e632f417478
Deathblood <deathbloodjr@gmail.com>
2025-03-13 10:03:13 -0500

    Updated for v2.0.0 of the game
    Basically completely rewrote this mod, since it has a completely different use case now.

--- CrownsOnSongSelect/Plugin.cs
+++ CrownsOnSongSelect/Plugin.cs
@@ -7,6 +7,8 @@ using BepInEx.Configuration;
 using CrownsOnSongSelect.Plugins;
 using UnityEngine;
 using System.Collections;
+using SaveProfileManager.Plugins;
+using System.Reflection;
 
 namespace CrownsOnSongSelect
 {
@@ -21,8 +23,11 @@ namespace CrownsOnSongSelect
 
 
         public ConfigEntry<bool> ConfigEnabled;
-        public ConfigEntry<string> ConfigSongTitleLanguageOverride;
-        public ConfigEntry<float> ConfigFlipInterval;
+        public ConfigEntry<bool> ConfigEnableEasy;
+        public ConfigEntry<bool> ConfigEnableNormal;
+        public ConfigEntry<bool> ConfigEnableHard;
+        public ConfigEntry<bool> ConfigEnableOni;
+        public ConfigEntry<bool> ConfigEnableUra;
 
 
 
@@ -32,18 +37,51 @@ namespace CrownsOnSongSelect
 
             Log = base.Log;
 
-            SetupConfig();
+            SetupConfig(Config, Path.Combine("BepInEx", "data", ModName));
             SetupHarmony();
+
+            var isSaveManagerLoaded = IsSaveManagerLoaded();
+            if (isSaveManagerLoaded)
+            {
+                AddToSaveManager();
+            }
         }
 
-        private void SetupConfig()
+        private void SetupConfig(ConfigFile config, string saveFolder, bool isSaveManager = false)
         {
             var dataFolder = Path.Combine("BepInEx", "data", ModName);
 
-            ConfigEnabled = Config.Bind("General",
-                "Enabled",
+            if (!isSaveManager)
+            {
+                ConfigEnabled = config.Bind("General",
+                   "Enabled",
+                   true,
+                   "Enables the mod.");
+            }
+            ConfigEnableEasy = config.Bind("General",
+                "EnableEasy",
                 true,
-                "Enables the mod.");
+                "Enables easy difficulty crowns being displayed.");
+
+            ConfigEnableNormal = config.Bind("General",
+                "EnableNormal",
+                true,
+                "Enables normal difficulty crowns being displayed.");
+
+            ConfigEnableHard = config.Bind("General",
+                "EnableHard",
+                true,
+                "Enables hard difficulty crowns being displayed.");
+
+            ConfigEnableOni = config.Bind("General",
+                "EnableOni",
+                true,
+                "Enables oni difficulty crowns being displayed.");
+
+            ConfigEnableUra = config.Bind("General",
+                "EnableUra",
+                true,
+                "Enables ura difficulty crowns being displayed.");
         }
 
         private void SetupHarmony()
@@ -51,30 +89,32 @@ namespace CrownsOnSongSelect
             // Patch methods
             _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
-            if (ConfigEnabled.Value)
+            LoadPlugin(ConfigEnabled.Value);
+        }
+
+        public static void LoadPlugin(bool enabled)
+        {
+            if (enabled)
             {
                 bool result = true;
                 // If any PatchFile fails, result will become false
-                result &= PatchFile(typeof(CrownsOnSongSelectPatch));
-                result &= PatchFile(typeof(CrownsOnSongSelectPatch.UiSongButtonSetupPatch));
-#if DEBUG
-                //result &= PatchFile(typeof(EnableAllIconsForTesting));
-#endif
+                result &= Instance.PatchFile(typeof(CrownsPatch));
+                result &= Instance.PatchFile(typeof(UiSongButtonSetupPatch));
                 if (result)
                 {
-                    Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+                    Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
                 }
                 else
                 {
-                    Log.LogError($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load.");
+                    Logger.Log($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load.", LogType.Error);
                     // Unload this instance of Harmony
                     // I hope this works the way I think it does
-                    _harmony.UnpatchSelf();
+                    Instance._harmony.UnpatchSelf();
                 }
             }
             else
             {
-                Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is disabled.");
+                Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} is disabled.");
             }
         }
 
@@ -88,14 +128,54 @@ namespace CrownsOnSongSelect
             {
                 _harmony.PatchAll(type);
 #if DEBUG
-                Log.LogInfo("File patched: " + type.FullName);
+                Logger.Log("File patched: " + type.FullName);
 #endif
                 return true;
             }
             catch (Exception e)
             {
-                Log.LogInfo("Failed to patch file: " + type.FullName);
-                Log.LogInfo(e.Message);
+                Logger.Log("Failed to patch file: " + type.FullName);
+                Logger.Log(e.Message);
+                return false;
+            }
+        }
+
+        public static void UnloadPlugin()
+        {
+            Instance._harmony.UnpatchSelf();
+            Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} has been unpatched.");
+        }
+
+        public static void ReloadPlugin()
+        {
+            // Reloading will always be completely different per mod
+            // You'll want to reload any config file or save data that may be specific per profile
+            // If there's nothing to reload, don't put anything here, and keep it commented in AddToSaveManager
+            //SwapSongLanguagesPatch.InitializeOverrideLanguages();
+            //TaikoSingletonMonoBehaviour<CommonObjects>.Instance.MyDataManager.MusicData.Reload();
+        }
+
+        public void AddToSaveManager()
+        {
+            // Add SaveDataManager dll path to your csproj.user file
+            // https://github.com/Deathbloodjr/RF.SaveProfileManager
+            var plugin = new PluginSaveDataInterface(MyPluginInfo.PLUGIN_GUID);
+            plugin.AssignLoadFunction(LoadPlugin);
+            plugin.AssignUnloadFunction(UnloadPlugin);
+            //plugin.AssignReloadSaveFunction(ReloadPlugin);
+            plugin.AssignConfigSetupFunction(SetupConfig);
+            plugin.AddToManager();
+        }
+
+        private bool IsSaveManagerLoaded()
+        {
+            try
+            {
+                Assembly loadedAssembly = Assembly.Load("com.DB.RF.SaveProfileManager");
+                return loadedAssembly != null;
+            }
+            catch
+            {
                 return false;
             }
         }
